151e63cd1cf31dacc8a14ec294bc45292a218ed6
minhazpro3 <minhazpro1@gmail.com>
2022-11-14 15:20:43 +0600

    project setup

---
+++ src/color/color.js
@@ -0,0 +1,12 @@
+const colors = {
+  main: "#48B600",
+  white: "#FFFFFF",
+  black: "#000000",
+  lightBlack: "#BCBCBC",
+  orange: "#FFCE31",
+  red: "#FF0000",
+  gray: "#F4F4F4",
+  subGreen: "#EEFA",
+  blue: "#3887F9",
+  paypal: "#FFB730",
+};
